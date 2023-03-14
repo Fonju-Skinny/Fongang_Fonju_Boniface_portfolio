1aa9b6bdc68be3248a5051b26af86e6ac16842bc
Fonju-Skinny <122148008+Fonju-Skinny@users.noreply.github.com>
2023-03-14 17:37:47 +0100

    continue

--- js/section.js
+++ js/section.js
@@ -5,11 +5,13 @@ var tabContents = document.getElementsByClassName("tab-contents")
 function openTab(tabName) {
     for(tabLink of tabLinks){
         tabLink.classList.remove("add-link")
+    header.classList.remove('active');
     }
 
     for(tabContent of tabContents){
         tabContent.classList.remove("active-tab")
     }
     event.currentTarget.classList.add("add-link")
+    // header.classList.add('active');
     document.getElementById(tabName).classList.add("active-tab")
 }
